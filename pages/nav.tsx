3da99f1388b3eb90157a0993b78497f7a9b15229
asuka yagi <yagi.asuka.0x06@gmail.com>
2021-10-09 15:57:24 +0900

    add pages
    - 404
    - about
    - nav

---
+++ pages/nav.tsx
@@ -0,0 +1,11 @@
+import React from 'https://esm.sh/react'
+
+export default function Nav() {
+  return (
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/blog/hello-world">Hello World</a></li>
+    </ul>
+  )
+}
